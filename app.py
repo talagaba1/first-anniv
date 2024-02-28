f89faa4626585bb670d7180635c287df11d08599
talagaba1 <161574203+talagaba1@users.noreply.github.com>
2024-02-28 16:16:12 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+st.set_page_config(page_title="to my miku", page_icon=":growing_heart:", layout = "wide")
+
+st.subheader("hi, miku ko ")
+st.title("here's to 365 days of love with you")
+st.write("hi bebe! i made this website for u!! a simple lsm would NOT be able to express/come close to how much i love you")
+st.write("""i love everything about you bebe. the way you fix your hair when we vc, the way your hair perfectly frames your face,
+         the way everything you do makes you 10X MORE ATTRACTIVE !!!! AKSJHFKAJSHFA i love your dimples soososososo much bebi, thats why i love making you smile.
+         i also love knowing im the reason behind your smiles, and knowing that u are also the reason behind mine. words will never be able to express how much i love you and appreciate you.
+         we've been together for a year na, pero i still cant deny na i have a big FAT CRUSH on you. i internally scream when you look at me, when you hold my hand, when you smile at me;
+         everything you do makes my heart skip a beat. being with you makes me feel two very opposite things. you make me feel at ease, while also making my heart want to jump out of my chest.
+         believe me when i say i love you, and that youre my favorite person. everything and everyone else goes on mute when im with you. when i feel cold, the thought of you is like 
+         a warm, thick wool sweater. but beyond the romance and the adventures, what truly amazes me is the growth we've experienced together. 
+         we've supported each other through thick and thin, celebrated victories, and lifted each other up in moments of doubt. 
+         with you by my side, i've learned the true meaning of partnership and unconditional love.
+         never go away, bebi. stay with me forever and ill do the same.""")
+
+with st.container():
+    st.write("--")
+    st.header("songs that i dedicate to u/remind me of u:")
+    st.write("[celeste - tothapi](https://open.spotify.com/track/5DzcYyuk3GDTfv6LP961ZM?si=1324f8b623524f73)")
+    st.write("[turning page - sleeping at last](https://open.spotify.com/track/2kfGoV9a5dbSKCNmUWH2ZF?si=1e13814adc474092)")
+    st.write("[baliw - sud](https://open.spotify.com/track/22UHq6BMRkUkhRJfZOENJr?si=9b6f85a157744ce1)")
+    st.write("[captivated - ivos](https://open.spotify.com/track/39SQ0CjIineoa2mbiJpZdk?si=87c276b4bfb74075)")
+    st.write("[hiraya - ratavii](https://open.spotify.com/track/5jfUcCB2fXpnqWzi7Nmp7H?si=03ee770b7ce1480b)")
+    st.write("[upuan - ben&ben](https://open.spotify.com/track/3k7SKhwbvE2lAisLx3jZWl?si=c96f83c4c3984822)")
+    st.write("[sunkissed - khai dreams](https://open.spotify.com/track/34RmzDXMv0ELSQTK4pNvX3?si=407c82b49e124fd6)")
+    st.write("[araw-araw - ben&ben](https://open.spotify.com/track/4rG3h1QwgjSSWz8EIjCFrm?si=6bcc15716f71438c)")
+    st.write("[pambihirang harana - cesca](https://open.spotify.com/track/4qquDjvZsAFHDPLZGZZtIK?si=ee93f181f5bc42c1)")
+    st.write("[bawat piyesa - munimuni](https://open.spotify.com/track/1tC2PLqLeJXt0VlgOYpc6m?si=e8e46f56842b436e)")
+    st.write("[paper rings - taylor swift](https://open.spotify.com/track/4y5bvROuBDPr5fuwXbIBZR?si=3a9e725c25e54dfb)")
+    
